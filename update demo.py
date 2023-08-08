34e16b6db8a9ed7aa7104ac4543b7164da946156
uzaif2389 <141730136+uzaif2389@users.noreply.github.com>
2023-08-08 17:16:17 +0530

    Delete update demo.py

--- update demo.py
+++
@@ -1,7 +0,0 @@
-#this is hello world will print in python
-print("hello world")
-#this is hello world will print in python
-print("hello#this is hello world will print in python
-print("hello world"#this is hello world will print in python
-print("hello world")) world")#this is hello world will print in python
-print("hello world")
